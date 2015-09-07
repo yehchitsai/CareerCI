00f33547ffbd45dfc9b1c4af8fbfafa5437a0f63
jackdog040506 <jackdog040506@gmail.com>
2015-09-07 16:48:46 +0800

    job2_1 can work

--- application/controllers/career.php
+++ application/controllers/career.php
@@ -24,8 +24,43 @@ class Career extends CI_Controller{
 		$result = $this->db->query($sql);
 		foreach ($result->result() as $row)
 		{
-			$td=$this->job_model->progress_bar($row->total);
-		   $obj=array("jt_name"=>$row->jt_name,"score"=>$row->total,"progress_bar"=>$td);
+			$val=$row->total;
+			$dom="<div class='progress' id='pog'>";
+			if ($val<=20) {
+				$dom.='<div class="progress-bar progress-bar-success" style="width: '.$val.'%"></div>';
+				$dom.='<div class="progress-bar progress-bar-empty" style="width: '.(100-$val).'%"><span style="color:black;">'.$val.'分</span></div></div>';
+			}
+			else if ($val>20&&$val<=40){
+				$dom.='<div class="progress-bar progress-bar-success" style="width:20%">0%~20%</div>';
+				$dom.='<div class="progress-bar progress-bar-info" style="width: '.($val-20).'%"></div>';
+				$dom.='<div class="progress-bar progress-bar-empty" style="width: '.(100-$val).'%"><span style="color:black;">'.$val.'分</span></div></div>';
+			}
+			else if ($val>40&&$val<=60){
+				$dom.='<div class="progress-bar progress-bar-success" style="width:20%">0%~20%</div>';
+				$dom.='<div class="progress-bar progress-bar-info" style="width:20%">20%~40%</div>';
+				$dom.='<div class="progress-bar progress-bar-danger" style="width: '.($val-40).'%"></div>';
+				$dom.='<div class="progress-bar progress-bar-empty" style="width: '.(100-$val).'%"><span style="color:black;">'.$val.'分</span></div></div>';
+			}
+			else if ($val>60&&$val<=80){
+				$dom.='<div class="progress-bar progress-bar-success" style="width:20%">0%~20%</div>';
+				$dom.='<div class="progress-bar progress-bar-info" style="width:20%">20%~40%</div>';
+				$dom.='<div class="progress-bar progress-bar-danger" style="width:20%">40%~60%</div>';
+				$dom.='<div class="progress-bar progress-bar-warning" style="width: '.($val-60).'%"></div>';
+				$dom.='<div class="progress-bar progress-bar-empty" style="width: '.(100-$val).'%"><span style="color:black;">'.$val.'分</span></div></div>';
+			}
+			else if ($val>80&&$val<100) {
+				$dom.='<div class="progress-bar progress-bar-success" style="width:20%">0%~20%</div>';
+				$dom.='<div class="progress-bar progress-bar-info" style="width:20%">20%~40%</div>';
+				$dom.='<div class="progress-bar progress-bar-danger" style="width:20%">40%~60%</div>';
+				$dom.='<div class="progress-bar progress-bar-warning" style="width: 20%">60%~80%</div>';
+				$dom.='<div class="progress-bar progress-bar-shiny" style="width: '.($val-80).'%"></div>';
+				$dom.='<div class="progress-bar progress-bar-empty" style="width: '.(100-$val).'%"><span style="color:black;">'.$val.'分</span></div></div>';
+			}
+			else if($val==100){
+				$dom.='<div class="progress-bar progress-bar-shiny" style="width: 20%">100分!</div></div>';
+				return $dom;
+			};
+		   $obj=array("jt_name"=>$row->jt_name,"score"=>$row->total,"progress_bar"=>$dom);
 		   $data[$a]=$obj;$a++;
 		}
 		$patten=array("job_query"=>$data);
--- application/views/p2_1_job.php
+++ application/views/p2_1_job.php
@@ -1,4 +1,10 @@
-﻿
+﻿<script>
+	$(document).ready(function(){
+		
+	})
+
+
+</script>
 <div data-role="fieldcontain">
 	<H3 align="center">請選擇你未來想要達成的職業</H3>
 	<ul id="jt" class="jtc" data-role="listview" data-filter="false" data-theme="a" style="margin-bottom: 50px;" data-split-icon="star" data-split-theme="a">
@@ -6,7 +12,8 @@
 		<li class="ui-btn">
 			<a href='#detailJob' >{jt_name}</a>
 			<a href='#' class='split-button-custom' onclick='alert("已成功追蹤");' data-role='button' data-icon='star' data-iconpos='notext'>追蹤</a><br/>
-			<a href='#score'></a><div class="bar_place">{progress_bar}</div>
+			<a href='#score'></a><div class="bar_place"></div>
+			{progress_bar}
 		</li>
 		{/job_query}
 	</ul>		
